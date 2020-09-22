61d64e445da6c9774bbc8d7f3d62d649615d1bc8
genesisviva <genesisviva@gmail.com>
2020-09-22 12:28:54 +0300

    Custom filter2D added

--- Lab2Git/Lab2.cpp
+++ Lab2Git/Lab2.cpp
@@ -7,57 +7,69 @@
 using namespace cv;
 using namespace std;
 
-void customBlurFunc(const Mat& source, Mat& result, double coefficient) {
-    int size = floor(1 / coefficient);
-    if (size / 2 == 0)
-        size = size - 1;
-    vector<double> kernel(size, coefficient);
-    filter2D(source, result, source.depth(), kernel, Point(-1, -1));
-}
-
-void customSobelFunc(const Mat& source, Mat& result) {
-    Mat grayTemp;
-    cvtColor(source, grayTemp, COLOR_BGR2GRAY);
-    vector<double> kernel = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
-    filter2D(grayTemp, result, source.depth(), kernel, Point(-1, -1));
-}
-
-void customFeldmanFunc(const Mat& source, Mat& result) {
-    Mat grayTemp;
-    cvtColor(source, grayTemp, COLOR_BGR2GRAY);
-    vector<double> kernel = { 3, 10, 3, 0, 0, 0, -3, -10, -3 };
-    filter2D(grayTemp, result, source.depth(), kernel, Point(-1, -1));
-}
-
-void customScharrFunc(const Mat& source, Mat& result) {
-    Mat grayTemp;
-    cvtColor(source, grayTemp, COLOR_BGR2GRAY);
-    vector<double> kernel = { 47, 162, 47, 0, 0, 0, -47, -162, -47 };
-    filter2D(grayTemp, result, source.depth(), kernel, Point(-1, -1));
+int xGradient(Mat image, int x, int y)
+{
+    return image.at<uchar>(y - 1, x - 1) +
+        2 * image.at<uchar>(y, x - 1) +
+        image.at<uchar>(y + 1, x - 1) -
+        image.at<uchar>(y - 1, x + 1) -
+        2 * image.at<uchar>(y, x + 1) -
+        image.at<uchar>(y + 1, x + 1);
 }
 
 
+
+
+void SobelTransformFilter(const Mat& initial, Mat& result) {
+    result = initial.clone();
+    int gx;
+    for (int y = 0; y < initial.rows; y++)
+        for (int x = 0; x < initial.cols; x++)
+            result.at<uchar>(y, x) = 0.0;
+    for (int y = 1; y < initial.rows - 1; y++) {
+        for (int x = 1; x < initial.cols - 1; x++) {
+            gx = xGradient(initial, x, y);
+            result.at<uchar>(y, x) = gx;
+        }
+    }
+}
+
+void customFilter3Colored(const Mat& initial, Mat& result, vector<double> kernel) {
+    result = initial;
+    for (int i = 1; i < result.rows - 1; i++) {
+        for (int j = 1; j < result.cols - 1; j++) {
+            result.at<Vec3b>(i, j)[0] = initial.at<Vec3b>(i - 1, j - 1)[0] * kernel[0] + initial.at<Vec3b>(i, j - 1)[0] * kernel[1] + initial.at<Vec3b>(i + 1, j - 1)[0] * kernel[2] + initial.at<Vec3b>(i - 1, j)[0] * kernel[3] + initial.at<Vec3b>(i, j)[0] * kernel[4] + initial.at<Vec3b>(i + 1, j)[0] * kernel[5] + initial.at<Vec3b>(i - 1, j + 1)[0] * kernel[6] + initial.at<Vec3b>(i, j + 1)[0] * kernel[7] + initial.at<Vec3b>(i + 1, j + 1)[0] * kernel[8];
+            result.at<Vec3b>(i, j)[1] = initial.at<Vec3b>(i - 1, j - 1)[1] * kernel[0] + initial.at<Vec3b>(i, j - 1)[1] * kernel[1] + initial.at<Vec3b>(i + 1, j - 1)[1] * kernel[2] + initial.at<Vec3b>(i - 1, j)[1] * kernel[3] + initial.at<Vec3b>(i, j)[1] * kernel[4] + initial.at<Vec3b>(i + 1, j)[1] * kernel[5] + initial.at<Vec3b>(i - 1, j + 1)[1] * kernel[6] + initial.at<Vec3b>(i, j + 1)[1] * kernel[7] + initial.at<Vec3b>(i + 1, j + 1)[1] * kernel[8];
+            result.at<Vec3b>(i, j)[2] = initial.at<Vec3b>(i - 1, j - 1)[2] * kernel[0] + initial.at<Vec3b>(i, j - 1)[2] * kernel[1] + initial.at<Vec3b>(i + 1, j - 1)[2] * kernel[2] + initial.at<Vec3b>(i - 1, j)[2] * kernel[3] + initial.at<Vec3b>(i, j)[2] * kernel[4] + initial.at<Vec3b>(i + 1, j)[2] * kernel[5] + initial.at<Vec3b>(i - 1, j + 1)[2] * kernel[6] + initial.at<Vec3b>(i, j + 1)[2] * kernel[7] + initial.at<Vec3b>(i + 1, j + 1)[2] * kernel[8];
+        }
+    }
+}
+
+void customBlurFunc(const Mat& source, Mat& result) {
+    vector<double> kernel = { 0.1,0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+    customFilter3Colored(source, result, kernel);
+}
 
 int main()
 {
     Mat original;
     original = imread("shakal.jpg", cv::IMREAD_COLOR);
-    Mat blurAPI;
-    Mat customBlur;
-    Mat customSobel;
-    Mat customFeldman;
-    Mat customScharr;
-    GaussianBlur(original, blurAPI, Size (7, 47), 0, 0);
-    customBlurFunc(original, customBlur, 0.1);
-    customSobelFunc(original, customSobel);
-    customFeldmanFunc(original, customFeldman);
-    customFeldmanFunc(original, customScharr);
     imshow("Original", original);
+
+    Mat blurAPI;
+    GaussianBlur(original, blurAPI, Size(7, 47), 0, 0);
     imshow("Blured", blurAPI);
+
+    Mat customBlur;
+    customBlurFunc(original, customBlur);
     imshow("CustomBlured", customBlur);
-    imshow("CustomGrad", customSobel);
-    imshow("CustomFeldman", customFeldman);
-    imshow("CustomScharr", customScharr);
+
+
+    Mat customSobel;
+    Mat src = imread("shakal.jpg", cv::IMREAD_GRAYSCALE);
+    SobelTransformFilter(src, customSobel);
+    imshow("CustomSobel", customSobel);
+
     waitKey(0);
 }
 
